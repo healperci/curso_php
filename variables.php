d28a912dd1a831b86977cfb0987afe7288b97fe2
H.Perez an.sistemas 0704 <an.sistemas@01.laboratoriossfc.com>
2024-02-29 12:58:08 -0500

    Curso en linea video 06 2

--- variables.php
+++ variables.php
@@ -1,21 +1,24 @@
 <?php
  
 #String: Cadena de texto.
-$nombre = "Carlos" . '\n';
+$nombre = "Carlos" ;
 # Integer: Numeros enteros
 $numero = 7;
 
 #Boolean: Verdadero o Falso.
 $verdadero = false;
     
-    echo $nombre . '\n';
-    echo $numero . '\n';
-    echo $verdadero . '\n';
-echo 'Hola, ' . $nombre;
-
+    echo $nombre ;
+    echo '<br>';
+    echo $numero ;
+    echo '<br>';
+    echo $verdadero ;
+    echo '<br>';
+echo "Hola . $nombre";
+echo '<br>';
 echo gettype($nombre);
-
-echo 'Hola todos' . '\n';
-
+echo '<br>';
+echo "Hola todos . \n";
+echo '<br>';
 ?>
 
